a8fee0c15469c84f472b1f92eedff403ea1c95f3
Kleber Lucas.G.S. <lenovomicrosof1@gmail.com>
2026-08-07 13:33:30 -0300

    refactor: remover verificação de dispositivos móveis e redução de movimento na inicialização da linha do tempo

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 gsap.registerPlugin(ScrollTrigger);
 
-const isTabletOrMobile = window.matchMedia("(max-width: 1024px)").matches || window.innerWidth < 1024;
-const reduceMotion = window.matchMedia("(prefers-reduced-motion: reduce)").matches;
+
 
 const video = document.getElementById("scroll-video");
 const video2 = document.getElementById("scroll-video2");
@@ -9,9 +8,6 @@ const video3 = document.getElementById("scroll-video3");
 const video4 = document.getElementById("scroll-video4");
 
 function initTimeline() {
-    if (reduceMotion || isTabletOrMobile) {
-        return;
-    }
 
     // 1. Estados Iniciais - Tudo que vem de baixo nasce no yPercent: 100
     gsap.set(".secao5", { opacity: 0, yPercent: 30 });
